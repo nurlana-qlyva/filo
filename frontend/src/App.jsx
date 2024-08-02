4c826ca59247bbe86619332d0655645798b7af30
Nurlana Quluyeva <81523883+nurlana-qlyva@users.noreply.github.com>
2024-08-02 17:07:36 +0300

    FILO: test server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,10 @@
 import { useEffect, useState } from 'react'
 
 function App() {
-  const [data, setData] = useState([])
-  
-  useEffect(() => {
-    fetch('http://localhost:8081/araclar')
-    .then(res => res.json())
-    .then(res => setData(res))
-  })
-console.log(data)
+
 
   return (
     <>
-      {data.map(item => item.plaka)}
       hello
     </>
   )
