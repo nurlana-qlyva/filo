532308610a73723dbe08aa0709615d60a47b7d6b
Nurlana Quluyeva <81523883+nurlana-qlyva@users.noreply.github.com>
2024-08-02 17:40:43 +0300

    FILO: test server

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
+const bodyParser = require("body-parser"); // Import body-parser
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const cors = require("cors");
 var whitelist = [
   "http://localhost:5173",
   "https://efilo.netlify.app",
