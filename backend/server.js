887a52e0e5397a1610c5d756421b9b75cfa08e13
Nurlana Quluyeva <81523883+nurlana-qlyva@users.noreply.github.com>
2024-08-04 08:47:49 +0300

    FILO: test server

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,10 @@ const app = express();
 app.use(cors());
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "localhost",
+  user: "root",
+  password: "0212",
+  database: "filo",
 });
 
 db.connect((err) => {
