1f3e0fa383828fec77ad870e80b002e1362e9797
Nurlana Quluyeva <81523883+nurlana-qlyva@users.noreply.github.com>
2024-08-08 10:21:59 +0300

    deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const response = axios.get(`${import.meta.env.REACT_APP_API_URL}/araclar`);
+    console.log(response.data);
+  }, [])
 
   return (
     <>
